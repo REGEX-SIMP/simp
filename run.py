1953a5e2af5d7049865b698ca6ce7f464ea96ba4
REGEX SIMP <146496992+REGEX-SIMP@users.noreply.github.com>
2023-09-30 21:01:17 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
-import platform
-print("JOIN FB GROUP")
+import platform, time
 os.system('clear')
+os.system('xdg-open https://chat.whatsapp.com/ETph2TkuyGaGgVqXdnwuNz')
+print("join WhatsApp gc for upcoming updates");time.sleep(5)
+
+
 os.system("git pull")
+o
 b = platform.architecture()[0]
 if b == '64bit':
     import regex64
